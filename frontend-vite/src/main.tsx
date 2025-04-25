f69ba0bf88fdc9560f9cad942e81bf4b3d3fdb10
David C Abejide <73208402+Abejide1@users.noreply.github.com>
2025-04-25 17:55:23 -0400

    UI changes 2

--- frontend-vite/src/main.tsx
+++ frontend-vite/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import '/fonts/grotesco.css';
+
 
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
